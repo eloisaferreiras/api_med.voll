2ad662c3ea218465c9cd7fa75c25ea077753a6fe
eloisaferreiras <alwayzasuzana17mp3@gmail.com>
2023-10-09 18:53:22 -0300

    update

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLOutput;
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -20,31 +18,36 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { // DDO é uma classe que representa um objeto de negócio e contém propriedades e métodos para acessar e manipular os dados associados a esse objeto.
+    @Transactional // usada para garantir que, se ocorrer algum erro durante a execução do método, todas as alterações feitas no banco de dados durante a transação serão revertidas.
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { // DDO é uma classe que representa um objeto de negócio e contém propriedades e métodos para acessar e manipular os dados associados a esse objeto.
         repository.save(new Medico(dados));
 
     }
 
-    @GetMapping // metodo de leitura, apenas carregando dados do banco de dados
-    public Page<DadosListagemMedico> listar( @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByActiveTrue(paginacao).map(DadosListagemMedico::new);
+    @GetMapping // metodo de leitura, apenas carrega os dados do banco de dados
+    public ResponseEntity<Page<DadosListagemMedico>> listar( @PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
         // o List representa uma coleção ordenada de elementos. fornece métodos para adicionar, remover, acessar e manipular os elementos da lista.(CRUD).
+        return  ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico))
 
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
 
     }
 
--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,11 +1,10 @@
 package med.voll.api.medico;
 
-import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
-    Page<Medico> findAllByActiveTrue(Pageable paginacao);
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
